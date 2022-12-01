c3e52189ecd82419cb2b25cad8c9d021ad7e1dd2
patrickkooijman <pwakooijman@gmail.com>
2022-12-01 20:42:57 +0100

    2015-day4

--- src/2015/main.ts
+++ src/2015/main.ts
@@ -1,6 +1,7 @@
 import {challengeOne as dayOneChallengeOne, challengeTwo as dayOneChallengeTwo} from './day1';
 import {challengeOne as dayTwoChallengeOne, challengeTwo as dayTwoChallengeTwo} from './day2';
 import {challengeOne as dayThreeChallengeOne, challengeTwo as dayThreeChallengeTwo} from './day3';
+import {challengeOne as dayFourChallengeOne, challengeTwo as dayFourChallengeTwo} from './day4';
 
 (await Promise.all([
     dayOneChallengeOne(),
@@ -8,6 +9,8 @@ import {challengeOne as dayThreeChallengeOne, challengeTwo as dayThreeChallengeT
     dayTwoChallengeOne(),
     dayTwoChallengeTwo(),
     dayThreeChallengeOne(),
-    dayThreeChallengeTwo()
+    dayThreeChallengeTwo(),
+    dayFourChallengeOne(),
+    dayFourChallengeTwo()
 ])).map(res => console.log(res));
 
